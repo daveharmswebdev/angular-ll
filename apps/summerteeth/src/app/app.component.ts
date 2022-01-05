9e9ab008cd821308c56318c068def267fae75101
Dave Harms <daveharmswebdev@gmail.com>
2022-01-05 06:07:02 -0600

    initial commit

---
+++ apps/summerteeth/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lunch-and-learn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'summerteeth';
+}
